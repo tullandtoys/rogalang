dcef645215ac3c70ee5faa78632c8a2d92e41279
John Hausberg Anfindsen <john.h.anfindsen@gmail.com>
2025-03-26 21:03:00 +0100

    test(backend): Add testing for helpers. #28128

---
+++ helpers/test/test.js
@@ -0,0 +1,6 @@
+import { isEven } from "../iseven.js";
+
+console.log(isEven(28)) // should output true
+console.log(isEven(1)) // should output false
+console.log(isEven(999)) // should output false
+console.log(isEven(2)) // should output true
